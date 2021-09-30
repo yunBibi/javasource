8d98d89e638762813bc021a1f4df391b262d5f97
hp87 <yunyw94@naver.com>
2021-09-30 15:58:27 +0900

    database 커넥션

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -1,5 +1,32 @@
 package dept;
 
-public class DeptDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DeptDAO {
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		String url = "jdbc:oracle:thin:@172.30.1.54";
+		String user = "c##scott";
+		String password = "tiger";
+		
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				return con;
+	}
+	
+	
+	
 }
