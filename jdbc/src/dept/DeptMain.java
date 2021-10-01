0ab17c65664d39c21b1422284d52cf00a3aa6846
hp87 <yunyw94@naver.com>
2021-10-01 11:18:36 +0900

    메인 메뉴화면 작성

--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -1,9 +1,16 @@
 package dept;
 
+import java.util.Scanner;
+
 public class DeptMain {
 
 	public static void main(String[] args) {
-		// 주석
+		boolean run = true;
+		Scanner sc = new Scanner(System.in);
+		
+		while(run) {
+			
+		}
 		
 		
 	}
