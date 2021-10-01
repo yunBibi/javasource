2b86ca584b5e6dff5818cee5deb8cd8015883202
hp87 <yunyw94@naver.com>
2021-10-01 09:51:44 +0900

    update 추가

--- jdbc/src/emp/EmpMain.java
+++ jdbc/src/emp/EmpMain.java
@@ -1,4 +1,4 @@
-  package emp;
+package emp;
 
 import java.util.List;
 import java.util.Scanner;
@@ -8,7 +8,7 @@ public class EmpMain {
 
 		boolean run = true;
 		Scanner sc = new Scanner(System.in);
-		
+
 		EmpDAO dao = new EmpDAO();
 
 		while (run) {
@@ -16,8 +16,9 @@ public class EmpMain {
 			System.out.println("1. 개별 조회");
 			System.out.println("2. 전체 조회");
 			System.out.println("3. 급여 오름차순 조회");
-			System.out.println("4. 금여 수정");
-			System.out.println("5. 종료");
+			System.out.println("4. 급여 수정");
+			System.out.println("5. 추가수당 수정");
+			System.out.println("6. 종료");
 			System.out.println("==========================");
 
 			System.out.print("입력 >> ");
@@ -27,64 +28,78 @@ public class EmpMain {
 			case 1:
 				System.out.print("조회할 사원 번호 입력 : ");
 				int empno = sc.nextInt();
-			    EmpDTO dto = dao.getRow(empno);
-			    
-			    if(dto == null) {
-			    	System.out.println("\n데이터 없음");
-			    } else {
-			    	System.out.println("\n사번\t이름\t직무\t매니저\t입사일\t\t급여\t보너스\t부서번호");
-			    	System.out.print(dto.getEmpno()+"\t");
-			    	System.out.print(dto.getEname()+"\t");
-			    	System.out.print(dto.getJob()+"\t");
-			    	System.out.print(dto.getMgr()+"\t");
-			    	System.out.print(dto.getHiredate()+"\t");
-			    	System.out.print(dto.getSal()+"\t");
-			    	System.out.print(dto.getComm()+"\t");
-			    	System.out.println(dto.getDeptno()+"\n");
-			    }
+				EmpDTO dto = dao.getRow(empno);
+
+				if (dto == null) {
+					System.out.println("\n데이터 없음");
+				} else {
+					System.out.println("\n사번\t이름\t직무\t매니저\t입사일\t\t급여\t보너스\t부서번호");
+					System.out.print(dto.getEmpno() + "\t");
+					System.out.print(dto.getEname() + "\t");
+					System.out.print(dto.getJob() + "\t");
+					System.out.print(dto.getMgr() + "\t");
+					System.out.print(dto.getHiredate() + "\t");
+					System.out.print(dto.getSal() + "\t");
+					System.out.print(dto.getComm() + "\t");
+					System.out.println(dto.getDeptno() + "\n");
+				}
 				break;
 
 			case 2:
 				List<EmpDTO> list = dao.select();
 				System.out.println("\n사번\t이름\t직무\t매니저\t입사일\t\t급여\t보너스\t부서번호");
 				// 리스트에 들어 있는 내용 출력
-				for(EmpDTO dto1 : list) {
-					System.out.print(dto1.getEmpno()+"\t");
-			    	System.out.print(dto1.getEname()+"\t");
-			    	System.out.print(dto1.getJob()+"\t");
-			    	System.out.print(dto1.getMgr()+"\t");
-			    	System.out.print(dto1.getHiredate()+"\t");
-			    	System.out.print(dto1.getSal()+"\t");
-			    	System.out.print(dto1.getComm()+"\t");
-			    	System.out.print(dto1.getDeptno()+"\n");
+				for (EmpDTO dto1 : list) {
+					System.out.print(dto1.getEmpno() + "\t");
+					System.out.print(dto1.getEname() + "\t");
+					System.out.print(dto1.getJob() + "\t");
+					System.out.print(dto1.getMgr() + "\t");
+					System.out.print(dto1.getHiredate() + "\t");
+					System.out.print(dto1.getSal() + "\t");
+					System.out.print(dto1.getComm() + "\t");
+					System.out.print(dto1.getDeptno() + "\n");
 				}
-				
+
 				dao.select();
 				break;
-				
-			case 3: 
+
+			case 3:
 				System.out.println("사원별 급여 오름차순 조회");
 				System.out.println("사원명\t급여");
 				list = dao.orderBySal();
-				for(EmpDTO dto2 : list) {
-					System.out.print(dto2.getEname()+"\t");
-					System.out.print(dto2.getSal()+"\n");
-					
+				for (EmpDTO dto2 : list) {
+					System.out.print(dto2.getEname() + "\t");
+					System.out.print(dto2.getSal() + "\n");
+
 				}
 				System.out.println();
-				
+
 				break;
-				
+
 			case 4:
-				empno = 1111;
-				int sal = 1500;
 				
+				System.out.print("\n수정 사원 번호 : ");
+				empno = sc.nextInt();
+				System.out.print("\n수정 급여 : ");
+				int sal = sc.nextInt();
+				boolean updateFlag = dao.updateSal(empno, sal);
+				System.out.println(updateFlag ? "업데이트 성공" : "업데이트 실패");
+				System.out.println();
+				break;
+
+			case 5:
 				
+				System.out.print("\n조건 급여 : ");
+				sal = sc.nextInt();
+				System.out.print("추가 수당 : ");
+				int comm = sc.nextInt();
+				updateFlag=dao.updateComm(comm, sal);
+				System.out.println(updateFlag?"업데이트 성공":"업데이트 실패");
 				break;
 				
-			case 5:
+			case 6:
 				run = false;
-				break; 
+				break;
 			default:
 				System.out.println("번호를 확인해 주세요.");
 				break;
